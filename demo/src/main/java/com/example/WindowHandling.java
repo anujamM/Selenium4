8ce3cb31fdf9f9885070f379b1e0650629e8e468
Anujam Mandal <34121897+anujamM@users.noreply.github.com>
2024-08-16 01:31:19 +0530

    WindowHandles

---
+++ demo/src/main/java/com/example/WindowHandling.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WindowHandling {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit
+
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        WebElement el = driver.findElement((By.xpath("(//input[@class='RNmpXc'])[2]")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@class='RNmpXc'])[2]")));
+
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.COMMAND).click(el).keyUp(Keys.CONTROL).build().perform();
+
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> itr = windows.iterator();
+
+        String parent = itr.next();
+        String child = itr.next();
+
+        driver.switchTo().window(child);
+        System.out.println(driver.findElement(By.className("hero-tag-carousel__collage")).isDisplayed());
+        driver.switchTo().window(parent);
+        System.out.println(driver.findElement(By.className("lnXdpd")).isDisplayed());
+        driver.quit();
+    }
+}
