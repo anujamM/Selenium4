59c561038768609936d1efd1a75d6eacbb731b65
Anujam Mandal <34121897+anujamM@users.noreply.github.com>
2024-08-15 20:58:04 +0530

    Update Main.java

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,7 +3,10 @@ package com.example;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -18,6 +21,7 @@ public class Main {
         System.out.println(driver.getCurrentUrl());
 
         WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Explicit
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//xpath")));
         driver.quit();
     }
 }
