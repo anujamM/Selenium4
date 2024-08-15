7371be5f9edb13ad6ed83c89a1cb98ae0f84626c
Anujam Mandal <34121897+anujamM@users.noreply.github.com>
2024-08-15 21:52:05 +0530

    Waits

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,11 +2,16 @@ package com.example;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -22,6 +27,22 @@ public class Main {
 
         WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Explicit
         explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//xpath")));
+
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class); // Polling - Fluent Wait
+
+        WebElement el = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                if (driver.findElement(By.xpath("//Xpath")).isDisplayed()) {
+                    return driver.findElement(By.xpath("//Xpath"));
+                } else {
+                    return null;
+                }
+            }
+        });
+
+        System.out.println(driver.findElement(By.xpath("//Xpath")).isDisplayed());
+
         driver.quit();
     }
 }
