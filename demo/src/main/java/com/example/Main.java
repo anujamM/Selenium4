850844a5b1981d7a2cc0094cdf6575bb487ed251
Anujam Mandal <34121897+anujamM@users.noreply.github.com>
2024-08-15 20:53:47 +0530

    Update Main.java

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,15 +1,23 @@
 package com.example;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Implicit
+
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
+
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Explicit
         driver.quit();
     }
 }
