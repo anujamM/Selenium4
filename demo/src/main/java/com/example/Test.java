debb3920e6ebedcbf6d8f17083f3d1159edf1c0d
Anujam Mandal <34121897+anujamM@users.noreply.github.com>
2025-07-15 22:40:47 +0530

    Create Test.java

---
+++ demo/src/main/java/com/example/Test.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+
+        driver.get("https://www.google.in");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        // driver.quit();
+    }
+}
