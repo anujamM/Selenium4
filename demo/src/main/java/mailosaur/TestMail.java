c41a36ea8b527b990761438002c0f7be33a6647e
Anujam Mandal <34121897+anujamM@users.noreply.github.com>
2025-07-17 14:59:22 +0530

    Update TestMail.java

--- demo/src/main/java/mailosaur/TestMail.java
+++ demo/src/main/java/mailosaur/TestMail.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import org.testng.annotations.Test;
 import com.mailosaur.MailosaurClient;
 import com.mailosaur.MailosaurException;
+import com.mailosaur.models.Link;
 import com.mailosaur.models.Message;
 import com.mailosaur.models.MessageSearchParams;
 import com.mailosaur.models.SearchCriteria;
@@ -27,19 +29,33 @@ public class TestMail {
 //	    Date testStartDate = sdf.parse("2025-07-17T13:00:00");
 	    
 	    Calendar cal = Calendar.getInstance();
-	    cal.add(Calendar.HOUR_OF_DAY, -2); // Subtract 2 hours
+	    cal.add(Calendar.HOUR_OF_DAY, -5); // Subtract 5 hours
 	    long testStartDate = cal.getTimeInMillis();
 	    
 	    params.withServer(serverId).withReceivedAfter(testStartDate);
 	    
 	    SearchCriteria criteria = new SearchCriteria();
-	    criteria.withSentTo("anything@" + serverDomain);
+//	    criteria.withSentTo("anything@" + serverDomain);
 	    criteria.withSentFrom("mailanujam@gmail.com");
 	   
 	    Message message = mailosaur.messages().get(params, criteria);
-	   
+	    
+	    System.out.println("--------Subject--------");
 	    System.out.println(message.subject());
 	    assertNotNull(message);
 	    assertEquals(message.subject().contains("View your Credit Report"), true);
+	    
+	    System.out.println("--------Body--------");
+	    System.out.println(message.text().body());
+	    
+	    System.out.println("--------Links--------");
+	    System.out.println(message.html().links());
+	    List<Link> links = message.html().links();
+	    
+	    for(Link l : links) {
+	    	System.out.println("Text:" + l.text());
+	    	System.out.println("Links:" + l.href());
+	    }
+	    
 	  }
 }
